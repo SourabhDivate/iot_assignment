a0734fcd4fbc16de8151a37a8edb0cc014ce0b37
Sourabh Divate <sourabhdivate2727@gmail.com>
2025-06-25 14:56:06 +0530

    fit_band

---
+++ Fit_band/server.py
@@ -0,0 +1,69 @@
+from flask import Flask, request
+from utils.database import execute_select_query
+from utils.database import execute_query
+import paho.mqtt.client as mqtt
+
+add = Flask(__name__)
+def on_publish(method, status):
+    print("message is publish\n")
+    publisher = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+    publisher.connect("localhost")
+    message = f"{method} : {status}"
+    publisher.publish("health/status", message)
+
+    publisher.disconnect()
+
+
+@add.route('/add', methods = ['POST'])
+def add_user():
+    publisher = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+    publisher.on_publish = request.method
+
+    name = request.form.get('name')
+    age = request.form.get('age')
+    city = request.form.get('city')
+    steps = request.form.get('steps')
+    pulse = request.form.get('pulse')
+    oxygen = request.form.get('oxygen')
+    temperature = request.form.get('temperature')
+    query = f"insert into health (name, age, city, steps, pulse, oxygen, temp) values ('{name}', {age}, '{city}', {steps}, {pulse}, {oxygen}, {temperature});"
+    execute_query(query)
+    on_publish("add", "success")
+
+    return f"{name} inserted successfully"
+
+@add.route('/getall', methods = ['GET'])
+def all_user():
+    query = f"select * from health;"
+    user = execute_select_query(query)
+    on_publish("all", "success")
+
+    return user
+
+@add.route("/info", methods = ['GET'])
+def user():
+    name = request.form.get('name')
+    query = f"select * from health where name = '{name}';"
+    user = execute_select_query(query)
+    on_publish("user", "success")
+
+    return user
+
+@add.route('/update', methods = ['PUT'])
+def update_city():
+    name = request.form.get('name')
+    city = request.form.get('city')
+    query = f"update health SET city = '{city}' where name = '{name}';"
+    execute_query(query)
+    on_publish("update", "success")
+    return f"{name} update successfully"
+
+@add.route('/maxsteps', methods = ['GET'])
+def max_steps():
+    query = f"select * from health where steps = (select MAX(steps) from health);"
+    maximum = execute_select_query(query)
+    on_publish("max", "success")
+    return maximum
+
+if __name__ == "__main__":
+    add.run(debug=True)
---
+++ Fit_band/utils/database.py
@@ -0,0 +1,33 @@
+import mysql.connector
+
+def execute_query(query):
+    connection = mysql.connector.connect(
+        host = "localhost",
+        port = 3306,
+        user = "root",
+        password = "Sourya@0145",
+        database = "iotdb"
+    )
+
+    cursor = connection.cursor()
+    cursor.execute(query)
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+
+def execute_select_query(query):
+    connection = mysql.connector.connect(
+        host = "localhost",
+        port = 3306,
+        user = "root",
+        password = "Sourya@0145",
+        database = "iotdb"
+    )
+
+    cursor = connection.cursor()
+    cursor.execute(query)
+    user = cursor.fetchall()
+    cursor.close()
+    connection.close()
+    return user
